edb18ad59123db9672ce58a7ebeaee8637942259
PlasmaGH <77991203+PlasmaGH@users.noreply.github.com>
2024-03-23 11:05:05 -0500

    Update Dial.lua
    Updated casing, added comments and added other general improvements.

--- Dial.lua
+++ Dial.lua
@@ -1,35 +1,26 @@
 local module = {}
-
 local maxRange = 9 -- Storing this as a variable so it's not some random "magic number"
 
-local BaseFrame = Instance.new("Frame") do
-	BaseFrame.BackgroundColor3 = Color3.fromRGB(27, 27, 27)
-	BaseFrame.BorderColor3 = Color3.fromRGB(35, 35, 35)
-	BaseFrame.Size = UDim2.fromOffset(35, 50)
-	BaseFrame.BorderSizePixel = 2
-	BaseFrame.ClipsDescendants = true
-	BaseFrame.AnchorPoint = Vector2.new(.5, .5)
-	BaseFrame.Position = UDim2.fromScale(.5, .5)
+local baseFrame = Instance.new("Frame") do
+	baseFrame.BackgroundColor3 = Color3.fromRGB(27, 27, 27)
+	baseFrame.BorderColor3 = Color3.fromRGB(35, 35, 35)
+	baseFrame.Size = UDim2.fromOffset(35, 50)
+	baseFrame.BorderSizePixel = 2
+	baseFrame.ClipsDescendants = true
+	baseFrame.AnchorPoint = Vector2.new(.5, .5)
+	baseFrame.Position = UDim2.fromScale(.5, .5)
 end
 
-local BaseNumberLabel = Instance.new("TextLabel") do
-	BaseNumberLabel.BackgroundTransparency = 1
-	BaseNumberLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-	BaseNumberLabel.TextStrokeTransparency = 0
-	BaseNumberLabel.Size = UDim2.fromScale(1, 1)
-	BaseNumberLabel.TextScaled = true
-	BaseNumberLabel.AnchorPoint =  Vector2.new(.5, 1)
+local baseNumberLabel = Instance.new("TextLabel") do
+	baseNumberLabel.BackgroundTransparency = 1
+	baseNumberLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+	baseNumberLabel.TextStrokeTransparency = 0
+	baseNumberLabel.Size = UDim2.fromScale(1, 1)
+	baseNumberLabel.TextScaled = true
+	baseNumberLabel.AnchorPoint =  Vector2.new(.5, 1)
 end
 
-local DefaultTween = TweenInfo.new(1, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
-
-local function get_Y_Position_From_Number_Chosen(frameNumber, numberChosen)
-	return 1 + frameNumber - numberChosen
-end
-
-local function number_To_Padded_String(number, length)
-	return string.format("%0" .. length .. "d", number)
-end
+local defaultTween = TweenInfo.new(1, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
 
 function module.new(slots : number, inital_Number : number, desired_Parent : Instance?, desired_Position : UDim2?, x_Padding : number?, tweenInfo : TweenInfo?)
 
@@ -37,32 +28,31 @@ function module.new(slots : number, inital_Number : number, desired_Parent : Ins
 
 	newDial.slotFrames = {}
 	newDial.slotsNum = slots
-	newDial.tweenInfo = tweenInfo or DefaultTween
+	newDial.tweenInfo = tweenInfo or defaultTween
 
 	local x_Padding = x_Padding or 50
-	local desired_Position = (desired_Position or UDim2.fromScale(.5, 0)) - UDim2.new(slots * BaseFrame.Size.X.Scale, slots * BaseFrame.Size.X.Offset) -- Had to make this a local variable because of an analysis warning.. prtty annoying
+	local desired_Position = (desired_Position or UDim2.fromScale(.5, 0)) - UDim2.new(slots * baseFrame.Size.X.Scale, slots * baseFrame.Size.X.Offset) -- Had to make this a local variable because of an analysis warning.. prtty annoying
 
 	local inital_Number = math.floor(math.abs(inital_Number))
 
 	for slotIndex = 1, slots do
 
-		local newFrame = BaseFrame:Clone()
+		local newFrame = baseFrame:Clone()
 		local frameNumbers = {}
 
 		local initalSlotNumber = 0
 
 		for i = 0, maxRange do
 
-			local textLabel = BaseNumberLabel:Clone()
+			local textLabel = baseNumberLabel:Clone()
 			textLabel.Text = tostring(i)
 
 			textLabel.Position = UDim2.fromScale(.5, 
 				1 + i - initalSlotNumber
 			)
 
-			textLabel.Parent = newFrame
 			textLabel.Name = i;
-
+			textLabel.Parent = newFrame
 			frameNumbers[i] = textLabel
 
 		end
@@ -90,22 +80,25 @@ end
 
 function module.update(newDial : any, number : number)
 
-	assert(newDial, "@newDial is missing.")
-	assert(typeof(number) ~= nil, "@number is missing")
+	assert(newDial, "@newDial is missing")
+	assert(typeof(number) ~= nil, "@number is incorrect or missing")
 
-	if not newDial.slotFrames then
+	if newDial.slotFrames == nil then
 		return
 	end
 
 	local slotsNum = newDial.slotsNum
 
-	number = math.floor(math.abs(number))
+	number = math.floor(math.abs(number)) -- Adjust number. Decimals and negative numbers will be set to 0.
 
-	local maxNumber = 10 ^ slotsNum - 1 -- Cant really find a better solution. Looks ugly but whatev
-	local isOverNumber =  (number > maxNumber)
+	local maxNumber = (10 ^ slotsNum - 1)
+	local isOverNumber =  (number > maxNumber) -- Number is greater than the number is can display.
 
-	local totalSlotFrames = #newDial.slotFrames
 	local tweens = {}
+
+	local function number_To_Padded_String(number, length)
+		return string.format("%0" .. length .. "d", number)
+	end
 
 	for slotIndex, frameData in newDial.slotFrames do
 
@@ -122,11 +115,9 @@ function module.update(newDial : any, number : number)
 		for i = 0, maxRange do
 
 			local tween = game.TweenService:Create(frameNumbers[i], newDial.tweenInfo, {
-
 				Position = UDim2.fromScale(.5,
 					1 + i - paddedFrameNumber
 				)
-
 			})
 
 			table.insert(tweens, tween)
